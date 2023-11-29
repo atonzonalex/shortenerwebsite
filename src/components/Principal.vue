a8de1ed6bcfcb13b8632083a98a04d103a0dceda
Alex Ronel <alexronelatonzon@gmail.com>
2023-11-29 16:05:14 +0100

    second commit

---
+++ src/components/Principal.vue
@@ -0,0 +1,11 @@
+<template>
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</template>
+<script setup></script>
+<style></style>
